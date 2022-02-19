b2a38cecafb27b96a21976618ea3b2f447df69a7
Max Nurzia <max.nurzia@gmail.com>
2022-02-19 15:12:16 -0500

    tests: add intelligent rune randomizer

---
+++ tests/test_helpers.c
@@ -0,0 +1,28 @@
+#include "test_helpers.h"
+
+re_rune re_rune_rand() {
+    int cls = RAND_PARAM(4);
+    re_rune ch = 0;
+    if (cls == 0) {
+        int sub = RAND_PARAM(3);
+        if (sub == 0) {
+            ch = RAND_PARAM(10) + '0';
+        } else if (sub == 1) {
+            ch = RAND_PARAM(26) + 'a';
+        } else if (sub == 2) {
+            ch = RAND_PARAM(26) + 'A';
+        }
+    } else if (cls == 1) {
+        ch = RAND_PARAM(0x800 - 0x80) + 0x80;
+    } else if (cls == 2) {
+        int sub = RAND_PARAM(2);
+        if (sub == 0) {
+            ch = RAND_PARAM(0xD800 - 0x800) + 0x800;
+        } else if (sub == 1) {
+            ch = RAND_PARAM(0x10000 - 0xE000) + 0xE000;
+        }
+    } else if (cls == 3) {
+        ch = RAND_PARAM(0x10FFFF - 0x10000) + 0x10000;
+    }
+    return ch;
+}
---
+++ tests/test_helpers.h
@@ -0,0 +1,8 @@
+#ifndef TEST_HELPERS_H
+#define TEST_HELPERS_H
+
+#include "test_harness.h"
+
+re_rune re_rune_rand(void);
+
+#endif
