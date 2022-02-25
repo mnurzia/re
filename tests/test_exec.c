93b69fa63ad630cd4f91fbe22d5867f4e46cc914
Max Nurzia <max.nurzia@gmail.com>
2022-02-25 14:22:34 -0500

    tests: comment out exec tests for some reason

--- tests/test_exec.c
+++ tests/test_exec.c
@@ -1,6 +1,13 @@
 #include "test_exec.h"
 
 TEST(t_exec_nfa) {
+    /*re reg;
+    re__str_view in_text;
+    re_init(&reg, "a*");
+    re__str_view_init_s(&in_text, "aaaaaa");
+    re__compile_regex(&reg.data->compile, &reg.data->ast_root, &reg.data->program);
+    re__exec_nfa(&reg.data->exec, &reg.data->program, re__ast_root_get_num_groups(&reg.data->ast_root), in_text);
+    re_destroy(&reg);*/
     PASS();
 }
 
