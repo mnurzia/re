1a541b8dd52be50c153a153e24a44f7ab2ffb5b9
Max Nurzia <max.nurzia@gmail.com>
2022-02-25 17:14:37 -0500

    tests: add compile_charclass init test

--- tests/test_compile_charclass.c
+++ tests/test_compile_charclass.c
@@ -1,6 +1,9 @@
 #include "test_compile_charclass.h"
 
 TEST(t_compile_charclass_init) {
+    re__compile_charclass cc;
+    re__compile_charclass_init(&cc);
+    re__compile_charclass_destroy(&cc);
     PASS();
 }
 
