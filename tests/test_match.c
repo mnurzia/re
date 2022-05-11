b77f5b18233f94427e5a0a13060375679715008f
Max Nurzia <{ID}+{username}@users.noreply.github.com>
2022-05-11 17:52:08 -0400

    tests: make match tests easier to write

--- tests/test_match.c
+++ tests/test_match.c
@@ -1,82 +1,57 @@
 #include "test_match.h"
 
-TEST(t_match_one_character_anchored_both_groups_none) {
+static int test_match(const char* regex, const char* text, char anchor) {
+    re_match_anchor_type anchor_type = RE_MATCH_UNANCHORED;
+    mn__str_view text_view;
     re reg;
-    re_span spans[1];
-    ASSERT(!re_init(&reg, "a"));
-    ASSERT_EQ(
-        re_match(
-            &reg, 
-            RE_MATCH_ANCHOR_BOTH, 
-            RE_MATCH_GROUPS_NONE, 
-            "a", 1, 
-            spans
-        ),
-        RE_MATCH
-    );
-    ASSERT_EQ(
-        re_match(
-            &reg, 
-            RE_MATCH_ANCHOR_BOTH, 
-            RE_MATCH_GROUPS_NONE, 
-            "b", 1, 
-            spans
-        ),
-        RE_NOMATCH
-    );
-    ASSERT_EQ(
-        re_match(
-            &reg, 
-            RE_MATCH_ANCHOR_BOTH, 
-            RE_MATCH_GROUPS_NONE, 
-            "bc", 1, 
-            spans
-        ),
-        RE_NOMATCH
-    );
+    re_error rv = 0;
+    mn__str_view_init_s(&text_view, text);
+    if (anchor == 'S') {
+        anchor_type = RE_MATCH_ANCHOR_START;
+    }
+    if (anchor == 'E') {
+        anchor_type = RE_MATCH_ANCHOR_END;
+    }
+    if (anchor == 'B') {
+        anchor_type = RE_MATCH_ANCHOR_BOTH;
+    }
+    if (anchor == 'U') {
+        anchor_type = RE_MATCH_UNANCHORED;
+    }
+    ASSERT(!re_init(&reg, regex));
+    rv = re_match(&reg, anchor_type, RE_MATCH_GROUPS_NONE, text, mn__str_view_size(&text_view), MN_NULL);
+    if (rv != RE_MATCH) {
+        ASSERT(rv == RE_NOMATCH);
+    }
     re_destroy(&reg);
-    PASS();
+    return rv == RE_MATCH;
 }
 
-TEST(t_match_one_character_anchored_start_groups_none) {
-    re reg;
-    re_span spans[1];
-    ASSERT(!re_init(&reg, "a"));
-    ASSERT_EQ(
-        re_match(
-            &reg, 
-            RE_MATCH_ANCHOR_START, 
-            RE_MATCH_GROUPS_NONE, 
-            "a", 1, 
-            spans
-        ),
-        RE_MATCH
-    );
-    ASSERT_EQ(
-        re_match(
-            &reg, 
-            RE_MATCH_ANCHOR_START, 
-            RE_MATCH_GROUPS_NONE, 
-            "abc", 1, 
-            spans
-        ),
-        RE_MATCH
-    );
-    ASSERT_EQ(
-        re_match(
-            &reg, 
-            RE_MATCH_ANCHOR_START, 
-            RE_MATCH_GROUPS_NONE, 
-            "ba", 1, 
-            spans
-        ),
-        RE_NOMATCH
-    );
-    re_destroy(&reg);
+TEST(t_match_one_character_groups_none) {
+    ASSERT(test_match("a", "a", 'S'));
+    ASSERT(test_match("a", "abc", 'S'));
+    ASSERT(!test_match("a", "bc", 'S'));
+    ASSERT(!test_match("a", "", 'S'));
+
+    ASSERT(test_match("a", "a", 'E'));
+    ASSERT(test_match("a", "ba", 'E'));
+    ASSERT(!test_match("a", "bac", 'E'));
+    ASSERT(!test_match("a", "", 'E'));
+    
+    ASSERT(test_match("a", "a", 'B'));
+    ASSERT(!test_match("a", "ba", 'B'));
+    ASSERT(!test_match("a", "ab", 'B'));
+    ASSERT(!test_match("a", "", 'B'));
+
+    ASSERT(test_match("a", "a", 'U'));
+    ASSERT(test_match("a", "ba", 'U'));
+    ASSERT(test_match("a", "bac", 'U'));
+    ASSERT(test_match("a", "cab", 'U'));
+    ASSERT(!test_match("a", "cb", 'U'));
+    ASSERT(!test_match("a", "", 'U'));
     PASS();
 }
 
 SUITE(s_match) {
-    RUN_TEST(t_match_one_character_anchored_both_groups_none);
-    RUN_TEST(t_match_one_character_anchored_start_groups_none);
+    RUN_TEST(t_match_one_character_groups_none);
 }
