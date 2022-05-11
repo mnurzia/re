d0d3b7fd3b1cb3ac1d2d0f70d8067744b5364245
Max Nurzia <{ID}+{username}@users.noreply.github.com>
2022-05-11 17:51:30 -0400

    tests: temporarily reorder match testing

--- tests/test_main.c
+++ tests/test_main.c
@@ -15,6 +15,7 @@ int main(void) {
     printf("\x1b[3J");
     MPTEST_MAIN_BEGIN();
     MPTEST_ENABLE_LEAK_CHECKING();
+    RUN_SUITE(s_match);
     RUN_SUITE(s_ast);
     RUN_SUITE(s_ast_root);
     RUN_SUITE(s_charclass);
@@ -22,7 +23,6 @@ int main(void) {
     RUN_SUITE(s_compile);
     RUN_SUITE(s_compile_charclass);
     /*RUN_SUITE(s_exec);*/
-    RUN_SUITE(s_match);
     RUN_SUITE(s_range);
     RUN_SUITE(s_parse);
     RUN_SUITE(s_prog_inst);
