33797c79f84e72f7c16060e6a960fe64b0f91f44
Max Nurzia <{ID}+{username}@users.noreply.github.com>
2022-05-17 21:27:03 -0400

    api: clean up function definitions
    These are still subject to change

--- re_api.c
+++ re_api.c
@@ -34,15 +34,24 @@ MN_INTERNAL void re__set_error_generic(re* reg, re_error err) {
     }
 }
 
-re_error re_init(re* reg, const char* regex) {
+re_error re_init(re* reg, const char* regex_nt) {
+    return re_init_sz_flags(reg, regex_nt, mn__str_slen(regex_nt), 0);
+}
+
+re_error re_init_flags(re* reg, const char* regex_nt, re_syntax_flags syntax_flags) {
+    return re_init_sz_flags(reg, regex_nt, mn__str_slen(regex_nt), syntax_flags);
+}
+
+re_error re_init_sz_flags(re* reg, const char* regex, mn_size regex_size, re_syntax_flags syntax_flags) {
     re_error err = RE_ERROR_NONE;
     mn__str_view regex_view;
+    MN__UNUSED(syntax_flags);
     reg->data = (re_data*)MN_MALLOC(sizeof(re_data));
     if (!reg->data) {
         return RE_ERROR_NOMEM;
     }
     re__error_init(reg);
-    mn__str_view_init_s(&regex_view, regex);
+    mn__str_view_init_n(&regex_view, regex, regex_size);
     re__parse_init(&reg->data->parse, reg);
     re__ast_root_init(&reg->data->ast_root);
     re__prog_init(&reg->data->program);
@@ -52,6 +61,11 @@ re_error re_init(re* reg, const char* regex) {
         return err;
     }
     return err;
+}
+
+re_error re_init_set(re* reg) {
+    MN__UNUSED(reg);
+    return RE_ERROR_NONE;
 }
 
 void re_destroy(re* reg) {
@@ -66,14 +80,14 @@ void re_destroy(re* reg) {
     }
 }
 
-const char* re_get_error(re* reg, mn_size* error_len) {
+const char* re_get_error(const re* reg, mn_size* error_len) {
     if (error_len != MN_NULL) {
         *error_len = mn__str_view_size(&reg->data->error_string_view);
     }
     return (const char*)mn__str_view_get_data(&reg->data->error_string_view);
 }
 
-mn_uint32 re_get_max_groups(re* reg) {
+mn_uint32 re_get_max_groups(const re* reg) {
     return re__ast_root_get_num_groups(&reg->data->ast_root);
 }
 
@@ -139,10 +153,17 @@ re_error re__match_prepare_progs(re* reg, int fwd, int rev, int fwd_dotstar, int
  * - Can bail early if highest-priority match state found? -> return match index, pos
  */
 
+#if MN_DEBUG
+
+#include <stdio.h>
+
+#endif
+
 re_error re__match_dfa_driver(
     re__prog* program, 
     re__prog_entry entry, 
     int request, /* 0 for boolean, 1 for match pos + index */
+    int bool_bail,
     int reversed,
     mn_size start_pos,
     const char* text, 
@@ -153,8 +174,8 @@ re_error re__match_dfa_driver(
     re_error err = RE_ERROR_NONE;
     re__exec_dfa exec_dfa;
     re__exec_dfa_start_state_flags start_state_flags =
-        RE__EXEC_DFA_FLAG_START_STATE_BEGIN_TEXT
-        | RE__EXEC_DFA_FLAG_START_STATE_BEGIN_LINE;
+        RE__EXEC_DFA_START_STATE_FLAG_BEGIN_TEXT
+        | RE__EXEC_DFA_START_STATE_FLAG_BEGIN_LINE;
     mn_size last_found_pos = 0;
     mn_uint32 last_found_match = 0;
     mn_uint32 match_status = 0;
@@ -166,17 +187,17 @@ re_error re__match_dfa_driver(
     MN_ASSERT(MN__IMPLIES(request, out_match != MN_NULL));
     MN_ASSERT(MN__IMPLIES(request, out_pos != MN_NULL));
     while (pos < text_size) {
-        char ch;
+        unsigned char ch = 0;
         if (!reversed) {
-            ch = text[pos];
+            ch = (unsigned char)(text[pos]);
         } else {
-            ch = text[(text_size - pos) - 1];
+            ch = (unsigned char)(text[(text_size - pos) - 1]);
         }
-        if ((err = re__exec_dfa_run(&exec_dfa, (mn_uint32)ch))) {
+        if ((err = re__exec_dfa_run(&exec_dfa, ch))) {
             goto err_destroy_dfa;
         }
         if (request == 0) {
-            if (re__exec_dfa_get_match_index(&exec_dfa)) {
+            if (re__exec_dfa_get_match_index(&exec_dfa) && bool_bail) {
                 goto match_early_boolean;
             }
         } else {
@@ -225,246 +246,209 @@ err_destroy_dfa:
     return err;
 }
 
-re_error re_match(re* reg, re_match_anchor_type anchor_type, re_match_groups_type groups_type, const char* string, mn_size string_size, re_span* out) {
+re_error re_is_match(re* reg, const char* text, mn_size text_size, re_anchor_type anchor_type) {
+    /* no groups -- dfa can be used in all cases */
     re_error err = RE_ERROR_NONE;
-    re__exec_nfa exec_nfa;
-    mn__str_view string_view;
-    if (groups_type < RE_MATCH_GROUPS_NONE) {
-        return RE_ERROR_INVALID;
-    }
-    if (groups_type > (re_match_groups_type)re__ast_root_get_num_groups(&reg->data->ast_root)) {
-        return RE_ERROR_INVALID;
-    }
-    if (groups_type == RE_MATCH_GROUPS_NONE) {
-        /* no groups -- dfa can be used in all cases */
-        if (anchor_type == RE_MATCH_ANCHOR_BOTH) {
-            /* use dfa to search forward.
-             * - can bail early if no threads left
-             * - cannot bail early if match state found (does not need to check for match every byte) */
-            if ((err = re__match_prepare_progs(reg, 1, 0, 0, 0))) {
-                return err;
-            }
-            return re__match_dfa_driver(
-                &reg->data->program,
-                RE__PROG_ENTRY_DEFAULT,
-                0,
-                0,
-                0,
-                string,
-                string_size,
-                MN_NULL,
-                MN_NULL
-            );
-        } else if (anchor_type == RE_MATCH_ANCHOR_START) {
-            /* use dfa to search forward.
-             * - can bail early if no threads left
-             * - can bail early if match state found */
-            if ((err = re__match_prepare_progs(reg, 1, 0, 0, 0))) {
-                return err;
-            }
-            return re__match_dfa_driver(
-                &reg->data->program,
-                RE__PROG_ENTRY_DEFAULT,
-                0,
-                0,
-                0,
-                string,
-                string_size,
-                MN_NULL,
-                MN_NULL
-            );
-        } else if (anchor_type == RE_MATCH_ANCHOR_END) {
-            if ((err = re__match_prepare_progs(reg, 0, 1, 0, 0))) {
-                return err;
-            }
-            return re__match_dfa_driver(
-                &reg->data->program_reverse,
-                RE__PROG_ENTRY_DEFAULT,
-                0,
-                1,
-                0,
-                string,
-                string_size,
-                MN_NULL,
-                MN_NULL
-            );
-        } else if (anchor_type == RE_MATCH_UNANCHORED) {
-            if ((err = re__match_prepare_progs(reg, 1, 0, 1, 0))) {
-                return err;
-            }
-            return re__match_dfa_driver(
-                &reg->data->program,
-                RE__PROG_ENTRY_DOTSTAR,
-                0,
-                0,
-                0,
-                string,
-                string_size,
-                MN_NULL,
-                MN_NULL
-            );
+    if (anchor_type == RE_ANCHOR_BOTH) {
+        /* use dfa to search forward.
+            * - can bail early if no threads left
+            * - cannot bail early if match state found (does not need to check for match every byte) */
+        if ((err = re__match_prepare_progs(reg, 1, 0, 0, 0))) {
+            return err;
         }
-    } else if (groups_type == RE_MATCH_GROUPS_MATCH_BOUNDARIES) {
-        /* match boundaries -- dfa can be used in all cases */
+        return re__match_dfa_driver(
+            &reg->data->program,
+            RE__PROG_ENTRY_DEFAULT,
+            0, 0, 0, 0,
+            text,
+            text_size,
+            MN_NULL,
+            MN_NULL
+        );
+    } else if (anchor_type == RE_ANCHOR_START) {
+        /* use dfa to search forward.
+            * - can bail early if no threads left
+            * - can bail early if match state found */
+        if ((err = re__match_prepare_progs(reg, 1, 0, 0, 0))) {
+            return err;
+        }
+        return re__match_dfa_driver(
+            &reg->data->program,
+            RE__PROG_ENTRY_DEFAULT,
+            0, 1, 0, 0,
+            text,
+            text_size,
+            MN_NULL,
+            MN_NULL
+        );
+    } else if (anchor_type == RE_ANCHOR_END) {
+        if ((err = re__match_prepare_progs(reg, 0, 1, 0, 0))) {
+            return err;
+        }
+        return re__match_dfa_driver(
+            &reg->data->program_reverse,
+            RE__PROG_ENTRY_DEFAULT,
+            0, 1, 1, 0,
+            text,
+            text_size,
+            MN_NULL,
+            MN_NULL
+        );
+    } else if (anchor_type == RE_UNANCHORED) {
+        if ((err = re__match_prepare_progs(reg, 1, 0, 1, 0))) {
+            return err;
+        }
+        return re__match_dfa_driver(
+            &reg->data->program,
+            RE__PROG_ENTRY_DOTSTAR,
+            0, 1, 0, 0,
+            text,
+            text_size,
+            MN_NULL,
+            MN_NULL
+        );
+    } else {
+        return RE_ERROR_INVALID;
+    }
+}
+
+re_error re_match_groups(re* reg, const char* text, mn_size text_size, re_anchor_type anchor_type, mn_uint32 max_group, re_span* out_groups) {
+    re_error err = RE_ERROR_NONE;
+    if (max_group == 0) {
+        return re_is_match(reg, text, text_size, anchor_type);
+    } else if (max_group == 1) {
         mn_uint32 out_match;
         mn_size out_pos;
         re_error match_err;
-        if (anchor_type == RE_MATCH_ANCHOR_BOTH) {
+        if (anchor_type == RE_ANCHOR_BOTH) {
             if ((err = re__match_prepare_progs(reg, 1, 0, 0, 0))) {
                 return err;
             }
             match_err = re__match_dfa_driver(
                 &reg->data->program,
                 RE__PROG_ENTRY_DEFAULT,
-                1,
-                0,
-                0,
-                string,
-                string_size,
+                1, 0, 0, 0,
+                text,
+                text_size,
                 &out_match,
                 &out_pos
             );
             if (match_err == RE_MATCH) {
-                if (out_pos != string_size) {
+                if (out_pos != text_size) {
                     return RE_NOMATCH;
                 } else {
-                    out[0].begin = 0;
-                    out[0].end = out_pos;
+                    out_groups[0].begin = 0;
+                    out_groups[0].end = out_pos;
                     return RE_MATCH;
                 }
             } else {
                 return match_err;
             }
-        } else if (anchor_type == RE_MATCH_ANCHOR_START) {
+        } else if (anchor_type == RE_ANCHOR_START) {
             if ((err = re__match_prepare_progs(reg, 1, 0, 0, 0))) {
                 return err;
             }
             match_err = re__match_dfa_driver(
                 &reg->data->program,
                 RE__PROG_ENTRY_DEFAULT,
-                1,
-                0,
-                0,
-                string,
-                string_size,
+                1, 0, 0, 0,
+                text,
+                text_size,
                 &out_match,
                 &out_pos
             );
             if (match_err == RE_MATCH) {
-                out[0].begin = 0;
-                out[0].end = out_pos;
+                out_groups[0].begin = 0;
+                out_groups[0].end = out_pos;
                 return RE_MATCH;
             } else {
                 return match_err;
             }
-        } else if (anchor_type == RE_MATCH_ANCHOR_END) {
+        } else if (anchor_type == RE_ANCHOR_END) {
             if ((err = re__match_prepare_progs(reg, 0, 1, 0, 0))) {
                 return err;
             }
             match_err = re__match_dfa_driver(
                 &reg->data->program_reverse,
                 RE__PROG_ENTRY_DEFAULT,
-                1,
-                1,
-                0,
-                string,
-                string_size,
+                1, 0, 1, 0,
+                text,
+                text_size,
                 &out_match,
                 &out_pos
             );
             if (match_err == RE_MATCH) {
-                out[0].begin = out_pos;
-                out[0].end = string_size;
+                out_groups[0].begin = out_pos;
+                out_groups[0].end = text_size;
                 return RE_MATCH;
             } else {
                 return match_err;
             }
-        } else if (anchor_type == RE_MATCH_UNANCHORED) {
+        } else if (anchor_type == RE_UNANCHORED) {
             if ((err = re__match_prepare_progs(reg, 1, 1, 1, 0))) {
                 return err;
             }
             match_err = re__match_dfa_driver(
                 &reg->data->program,
                 RE__PROG_ENTRY_DOTSTAR,
-                1,
-                0,
-                0,
-                string,
-                string_size,
+                1, 0, 0, 0,
+                text,
+                text_size,
                 &out_match,
                 &out_pos
             );
             if (match_err == RE_MATCH) {
-                out[0].end = out_pos;
+                out_groups[0].end = out_pos;
                 /* scan back to find the start */
                 match_err = re__match_dfa_driver(
                     &reg->data->program_reverse,
                     RE__PROG_ENTRY_DEFAULT,
-                    1,
-                    1,
-                    out_pos,
-                    string,
-                    string_size,
+                    1, 0, 1, out_pos,
+                    text,
+                    text_size,
                     &out_match,
                     &out_pos
                 );
                 /* should ALWAYS match. */
                 MN_ASSERT(match_err == RE_MATCH);
-                out[0].begin = out_pos;
+                out_groups[0].begin = out_pos;
                 return RE_MATCH;
             } else {
                 return match_err;
             }
+        } else {
+            return RE_ERROR_INVALID;
         }
     } else {
-        /* subgroups -- dfa may be used to found boundaries */
-        if (groups_type == RE_MATCH_GROUPS_ALL) {
-            groups_type = (re_match_groups_type)re__ast_root_get_num_groups(&reg->data->ast_root);
-        }
-        if (anchor_type == RE_MATCH_ANCHOR_BOTH) {
-            if ((err = re__match_prepare_progs(reg, 1, 0, 0, 0))) {
-                return err;
+        re__exec_nfa exec_nfa;
+        mn__str_view string_view;
+        re__exec_nfa_init(&exec_nfa, &reg->data->program, max_group + 1);
+        mn__str_view_init_n(&string_view, text, text_size);
+        {
+            mn_size pos;
+            re__ast_assert_type assert_ctx = RE__AST_ASSERT_TYPE_TEXT_START | RE__AST_ASSERT_TYPE_TEXT_START_ABSOLUTE;
+            if ((err = re__exec_nfa_start(&exec_nfa, assert_ctx, 1))) {
+                goto error;
             }
-        } else if (anchor_type == RE_MATCH_ANCHOR_START) {
-            if ((err = re__match_prepare_progs(reg, 1, 0, 0, 0))) {
-                return err;
-            }
-        } else if (anchor_type == RE_MATCH_ANCHOR_END) {
-            if ((err = re__match_prepare_progs(reg, 0, 1, 0, 0))) {
-                return err;
-            }
-        } else if (anchor_type == RE_MATCH_UNANCHORED) {
-            if ((err = re__match_prepare_progs(reg, 1, 1, 1, 0))) {
-                return err;
-            }
-        }
-    }
-    re__exec_nfa_init(&exec_nfa, &reg->data->program, groups_type);
-    mn__str_view_init_n(&string_view, string, string_size);
-    {
-        mn_size pos;
-        re__ast_assert_type assert_ctx = RE__AST_ASSERT_TYPE_TEXT_START | RE__AST_ASSERT_TYPE_TEXT_START_ABSOLUTE;
-        if ((err = re__exec_nfa_start(&exec_nfa, assert_ctx, 1))) {
-            goto error;
-        }
-        for (pos = 0; pos < mn__str_view_size(&string_view); pos++) {
-            mn_char ch = mn__str_view_get_data(&string_view)[pos];
-            mn_uint32 match;
-            assert_ctx = re__match_next_assert_ctx(pos, mn__str_view_size(&string_view));
-            if ((err = re__exec_nfa_run(&exec_nfa, ch, pos, assert_ctx))) {
-                return err;
-            }
-            match = re__exec_nfa_get_match_index(&exec_nfa);
-            if (match) {
-                if (anchor_type == RE_MATCH_ANCHOR_BOTH || anchor_type == RE_MATCH_ANCHOR_END) {
-                    /* can bail! */
-                    break;
+            for (pos = 0; pos < mn__str_view_size(&string_view); pos++) {
+                mn_uint8 ch = (mn_uint8)mn__str_view_get_data(&string_view)[pos];
+                mn_uint32 match;
+                assert_ctx = re__match_next_assert_ctx(pos, mn__str_view_size(&string_view));
+                if ((err = re__exec_nfa_run(&exec_nfa, ch, pos, assert_ctx))) {
+                    return err;
+                }
+                match = re__exec_nfa_get_match_index(&exec_nfa);
+                if (match) {
+                    if (anchor_type == RE_ANCHOR_BOTH || anchor_type == RE_ANCHOR_END) {
+                        /* can bail! */
+                        break;
+                    }
                 }
             }
+            err = re__exec_nfa_finish(&exec_nfa, out_groups, pos);
         }
-        err = re__exec_nfa_finish(&exec_nfa, out, pos);
+    error:
+        re__exec_nfa_destroy(&exec_nfa);
+        return err;
     }
-error:
-    re__exec_nfa_destroy(&exec_nfa);
-    return err;
 }
--- re_api.h
+++ re_api.h
@@ -31,30 +31,124 @@ typedef struct re_span {
     mn_size end;
 } re_span;
 
-#define RE__MATCH_GROUPS_MAX 10000
+typedef enum re_anchor_type {
+    RE_ANCHOR_BOTH = 'B',
+    RE_ANCHOR_START = 'S',
+    RE_ANCHOR_END = 'E',
+    RE_UNANCHORED = 'U'
+} re_anchor_type;
 
-typedef enum re_match_anchor_type {
-    RE_MATCH_ANCHOR_BOTH,
-    RE_MATCH_ANCHOR_START,
-    RE_MATCH_ANCHOR_END,
-    RE_MATCH_UNANCHORED
-} re_match_anchor_type;
+typedef enum re_syntax_flags {
+    RE_SYNTAX_FLAG_ASCII = 1,
+    RE_SYNTAX_FLAG_IGNORECASE = 2,
+    RE_SYNTAX_FLAG_MULTILINE = 4,
+    RE_SYNTAX_FLAG_DOTALL = 8
+} re_syntax_flags;
 
-typedef enum re_match_groups_type {
-    RE_MATCH_GROUPS_NONE = -1,
-    RE_MATCH_GROUPS_MATCH_BOUNDARIES = 0,
-    RE_MATCH_GROUPS_ALL = RE__MATCH_GROUPS_MAX
-} re_match_groups_type;
+/* Comparison of different matching APIs:
+ * PCRE2: 
+ *   int pcre2_match(
+ *     const pcre2_code *code,
+ *     PCRE2_SPTR subject,
+ *     PCRE2_SIZE length,
+ *     PCRE2_SIZE startoffset,
+ *     uint32_t options,
+ *     pcre2_match_data *match_data,
+ *     pcre2_match_context *mcontext);
+ * POSIX:
+ *   int regexec(
+ *     const regex_t *restrict preg, 
+ *     const char *restrict string,
+ *     size_t nmatch, 
+ *     regmatch_t pmatch[restrict], 
+ *     int eflags);
+ * GRegex:
+ *   gboolean g_regex_match (
+ *     const GRegex* regex,
+ *     const gchar* string,
+ *     GRegexMatchFlags match_options,
+ *     GMatchInfo** match_info);
+ *   gboolean
+ *     g_regex_match_all (
+ *     const GRegex* regex,
+ *     const gchar* string,
+ *     GRegexMatchFlags match_options,
+ *     GMatchInfo** match_info);
+ *   gboolean
+ *     g_regex_match_all_full (
+ *     const GRegex* regex,
+ *     const gchar* string,
+ *     gssize string_len,
+ *     gint start_position,
+ *     GRegexMatchFlags match_options,
+ *     GMatchInfo** match_info,
+ *     GError** error);
+ *   gboolean
+ *     g_regex_match_full (
+ *     const GRegex* regex,
+ *     const gchar* string,
+ *     gssize string_len,
+ *     gint start_position,
+ *     GRegexMatchFlags match_options,
+ *     GMatchInfo** match_info,
+ *     GError** error);
+ * Hyperscan:
+ *   hs_error_t hs_scan(
+ *     const hs_database_t *db,
+ *     const char *data,
+ *     unsigned int length,
+ *     unsigned int flags,
+ *     hs_scratch_t *scratch,
+ *     match_event_handler onEvent,
+ *     void *context);
+ *   hs_error_t hs_scan_vector(const hs_database_t *db,
+ *     const char *const *data,
+ *     const unsigned int *length,
+ *     unsigned int count, 
+ *     unsigned int flags,
+ *     hs_scratch_t *scratch,
+ *     match_event_handler onEvent, 
+ *     void *context);
+ * RE2 is omitted because it takes advantage of C++ features.
+ */
 
-re_error re_init(re* reg, const char* regex);
-re_error re_init_set(re* reg, const char** regexes, mn_uint32 regexes_size);
+/* Inputs:
+ * Text (always)
+ * Text size (optional)
+ * Start anchoring (optional, implied anchored)
+ * End anchoring (optional, implied anchored)
+ * | Match result boolean
+ * | Match result boundaries
+ * | Match result some groups
+ * | Match result all groups
+ * Match index (set)
+ * Scan options
+ *
+ * Outputs:
+ * Match result boolean -> whether or not the string matched
+ * Match result boundaries -> the boundaries of the match
+ * Match result some groups -> the boundaries of the match, and optionally group boundaries
+ * Match result all groups -> the boundaries of the match and group boundaries
+ * Match index -> which index regex was matched
+ * Error value
+ * 
+ */
+re_error re_init(re* reg, const char* regex_nt);
+re_error re_init_flags(re* reg, const char* regex_nt, re_syntax_flags syntax_flags);
+re_error re_init_sz_flags(re* reg, const char* regex, mn_size regex_size, re_syntax_flags syntax_flags);
+re_error re_init_set(re* reg);
+
+re_error re_set_add(re* reg, const char* regex_nt);
+re_error re_set_add_flags(re* reg, const char* regex_nt, re_syntax_flags syntax_flags);
+re_error re_set_add_sz_flags(re* reg, const char* regex, mn_size regex_size, re_syntax_flags syntax_flags);
+
+re_error re_is_match(re* reg, const char* text, mn_size text_size, re_anchor_type anchor_type);
+re_error re_match_groups(re* reg, const char* text, mn_size text_size, re_anchor_type anchor_type, mn_uint32 max_group, re_span* out_groups);
+re_error re_match_groups_set(re* reg, const char* text, mn_size text_size, re_anchor_type anchor_type, mn_uint32 max_group, re_span* out_groups, mn_uint32* out_set_index);
+
+const char* re_get_error(const re* reg, mn_size* error_len);
+mn_uint32 re_get_max_groups(const re* reg);
+
 void re_destroy(re* reg);
-
-const char* re_get_error(re* reg, mn_size* error_len);
-
-mn_uint32 re_get_max_groups(re* reg);
-
-re_error re_match(re* reg, re_match_anchor_type anchor_type, re_match_groups_type groups_type, const char* string, mn_size string_size, re_span* out);
-re_error re_match_set(re* reg, re_match_anchor_type anchor_type, re_match_groups_type groups_type, const char* const* strings, mn_size* string_sizes, mn_uint32* out_match_index, re_span* out);
 
 #endif
