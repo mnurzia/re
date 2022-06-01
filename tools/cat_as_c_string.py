e87e27e4d6c150f3c20cc9e62ff7efed56797fc4
Max Nurzia <{ID}+{username}@users.noreply.github.com>
2022-06-01 12:09:32 -0400

    tools: add utility to convert llvm fuzz output

---
+++ tools/cat_as_c_string.py
@@ -0,0 +1,26 @@
+import sys
+
+def escape(c):
+    return c.enco
+
+with open(sys.argv[1], 'rb') as f:
+    s = '"'
+    for c in f.read():
+        if c == 0x00:
+            s += "\\0"
+        elif c == 0x09:
+            s += "\\t"
+        elif c == 0x0A:
+            s += "\\n"
+        elif c == 0x0D:
+            s += "\\r"
+        elif c == 0x5C:
+            s += "\\\\"
+        elif c == 0x22:
+            s += "\\\""
+        elif c < 0x20 or c >= 0x7F:
+            s += "\\x%02X" % c
+        else:
+            s += chr(c)
+    s += '"'
+    print(s)
