afe3906e54e01bd604096cfc010d247eaf37cb37
Max Nurzia <{ID}+{username}@users.noreply.github.com>
2022-06-01 12:09:56 -0400

    tests: fix date

--- tests/test_regr.c
+++ tests/test_regr.c
@@ -2,7 +2,7 @@
 
 TEST(t_regr_parse_fuzz) {
     re reg;
-    /* 2022-05-04: libfuzzer */
+    /* 2022-05-24: libfuzzer */
     re_init_sz_flags(&reg, "\x28\x00\x00\x3f", 4, 0);
     re_destroy(&reg);
     re_init_sz_flags(&reg, "|\xf8$\xe5", 4, 0);
