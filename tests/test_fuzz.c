cac7cd151d85373f529fd2146d4c77c1d8d6f5fe
Max Nurzia <{ID}+{username}@users.noreply.github.com>
2022-06-01 12:09:13 -0400

    tests: start compiling fuzzed stuff

--- tests/test_fuzz.c
+++ tests/test_fuzz.c
@@ -7,6 +7,7 @@ typedef unsigned char uint8_t;
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   re reg;
   re_init_sz_flags(&reg, (const char*)Data, Size, 0);
+  re_is_match(&reg, "", 0, 'U');
   re_destroy(&reg);
   return 0;
 }
