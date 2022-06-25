6f48847f6a14ba089c8deb4bc9eca9dd2c20413d
Max Nurzia <{ID}+{username}@users.noreply.github.com>
2022-06-25 12:38:30 -0400

    build: adapt coverage runner to be more usable in vscode

--- tools/generate_coverage.sh
+++ tools/generate_coverage.sh
@@ -5,9 +5,9 @@ rm -f cov.profraw
 rm -f oom.profraw
 rm -f cov.profdata
 rm -f lcov.info
-$1 -s $COV_SUITE_NAME
+$1 -s $(echo $2 | sed 's/test_/s_/g')
 mv default.profraw cov.profraw
-$1 -s $COV_SUITE_NAME --leak-check-oom
+$1 -s $(echo $2 | sed 's/test_/s_/g') --leak-check-oom
 mv default.profraw oom.profraw
 llvm-profdata merge cov.profraw oom.profraw -o cov.profdata
 llvm-cov export re_cov -instr-profile=cov.profdata -format=lcov > lcov.info
